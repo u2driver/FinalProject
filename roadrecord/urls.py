394634d9b0fd612df493eb7b8089f14600b90fcc
u2driver <david_wright@natronaschools.org>
2022-11-18 14:37:13 -0700

    add record template and view

--- roadrecord/urls.py
+++ roadrecord/urls.py
@@ -4,4 +4,6 @@ from roadrecord import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("roadrecord/<name>", views.hello_there, name="hello_there"),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
 ]
--- roadrecord/views.py
+++ roadrecord/views.py
@@ -1,6 +1,7 @@
 import re
 from django.utils.timezone import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.template import loader
 from .models import Accidents
@@ -22,3 +23,17 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  v = request.POST['highway']
+  w = request.POST['month']
+  x = request.POST['day_night']
+  y = request.POST['marker']
+  z = request.POST['fatal_non']
+  crash = Accidents(highway = v, month = w, day_night = x, marker = y, fatal_non = z)
+  crash.save()
+  return HttpResponseRedirect(reverse('home'))
