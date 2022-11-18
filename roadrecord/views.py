9bdf659ca69fe8d969d16e145ed906fdb15e21be
u2driver <david_wright@natronaschools.org>
2022-11-18 13:59:53 -0700

    Updated templates

--- roadrecord/views.py
+++ roadrecord/views.py
@@ -2,9 +2,16 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Accidents
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+  crashes = Accidents.objects.all().values()
+  template = loader.get_template('home.html')
+  context = {
+    'crashes': crashes,
+  }
+  return HttpResponse(template.render(context, request))
 
 def hello_there(request, name):
     return render(
